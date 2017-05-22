fce0de2fddc68d0cb769e6a05b79c84584b6cfa1
Iurii Medvedev <iurii_medvedev@epam.com>
2017-05-22 11:38:31 +0300

    change disk type

--- disk.go
+++ disk.go
@@ -13,7 +13,7 @@ func MakeDiskImage(dest string, size uint, r io.Reader) error {
 	// Convert a raw image from stdin to the dest VMDK image.
 	sizeBytes := int64(size) << 20 // usually won't fit in 32-bit int (max 2GB)
 	cmd := exec.Command(VBM, "convertfromraw", "stdin", dest,
-		fmt.Sprintf("%d", sizeBytes), "--format", "VMDK")
+		fmt.Sprintf("%d", sizeBytes), "--format", "VDI")
 
 	if Verbose {
 		cmd.Stdout = os.Stdout
