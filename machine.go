f73cdf26536c821502c252cdeb94955cacbf9e7c
Maximilian Zollneritsch <m.zollneritsch@netconomy.net>
2017-10-02 11:18:17 +0200

    Added SetHostOnlyAdapter function to machine

--- machine.go
+++ machine.go
@@ -368,6 +368,11 @@ func (m *Machine) SetNIC(n int, nic NIC) error {
 	return vbm(args...)
 }
 
+// SetHostonlyAdapter set the n-th HostOnlyAdapter.
+func (m *Machine) SetHostonlyAdapter(n int, network HostonlyNet) error {
+	return vbm("modifyvm", "--hostonlyadapter", string(n), network.Name)
+}
+
 // AddStorageCtl adds a storage controller with the given name.
 func (m *Machine) AddStorageCtl(name string, ctl StorageController) error {
 	args := []string{"storagectl", m.Name, "--name", name}
