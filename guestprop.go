0b4f5495e6304f65f8377bc2d9bc0fc8f71513c3
Francois-Xavier 'FiX' KOWALSKI <francois-xavier.kowalski@hp.com>
2018-04-20 11:53:23 +0200

    Add base support to set & get guest properties from the host

---
+++ guestprop.go
@@ -0,0 +1,40 @@
+package virtualbox
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+)
+
+var (
+	getRegexp = regexp.MustCompile("Value: (.*)$")
+)
+
+// SetGuestProperty writes a VirtualBox guestproperty to the given value.
+func SetGuestProperty(vm string, prop string, val string) error {
+	return vbm("guestproperty", "set", vm, prop, val)
+}
+
+// GetGuestProperty reads a VirtualBox guestproperty.
+func GetGuestProperty(vm string, prop string) (string, error) {
+	var out string
+	var err error
+	out, err = vbmOut("guestproperty", "get", vm, prop)
+	if err != nil {
+		log.Print(err)
+		return "", err
+	}
+	log.Printf("out: '%s'", out)
+	if Verbose {
+		log.Printf("out (trimmed): '%s'", out)
+	}
+	var match = getRegexp.FindStringSubmatch(out)
+	if Verbose {
+		log.Print("match:", match)
+	}
+	if len(match) != 2 {
+		return "", fmt.Errorf("No match with VBoxManage get guestproperty output")
+	} else {
+		return match[1], nil
+	}
+}
