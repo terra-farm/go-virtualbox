f71c597627376f11adb3add4f08ddf576007fd5e
Francois-Xavier 'FiX' KOWALSKI <francois-xavier.kowalski@hp.com>
2018-04-23 17:41:25 +0200

    Add test data

--- machine_test.go
+++ machine_test.go
@@ -2,6 +2,9 @@ package virtualbox
 
 import "testing"
 
+var machineList = "\"Ubuntu\" {2e16b1fc-675d-4a7a-a9a1-e89a8bde7874}\n" +
+	"\"go-virtualbox\" {def44546-e3da-4902-8d15-b91c99c80cbc}"
+
 func TestMachine(t *testing.T) {
 	ms, err := ListMachines()
 	if err != nil {
