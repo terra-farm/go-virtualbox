576b87bfd891208c25a3292f3e1cfc5b1aed5f4c
iSage <isage.dna@gmail.com>
2019-10-23 12:55:38 +0300

    machine.go - add busy wait on poweroff, so machine can be reliably deleted afterwards

--- machine.go
+++ machine.go
@@ -153,7 +153,16 @@ func (m *Machine) Poweroff() error {
 	case Poweroff, Aborted, Saved:
 		return nil
 	}
-	return vbm("controlvm", m.Name, "poweroff")
+	for m.State != Poweroff { // busy wait until the machine is stopped, because it can lock machine deletion otherwise
+		if err := vbm("controlvm", m.Name, "poweroff"); err != nil {
+			return err
+		}
+		time.Sleep(1 * time.Second)
+		if err := m.Refresh(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // Restart gracefully restarts the machine.
